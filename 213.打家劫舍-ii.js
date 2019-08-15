2cfe2b58a53616dcc18fbb2ed2ccdb725ff852ca
jocs <ransixi@gmail.com>
2019-08-15 22:15:36 +0800

    Solve: 打家劫舍2

---
+++ 213.打家劫舍-ii.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=213 lang=javascript
+ *
+ * [213] 打家劫舍 II
+ *
+ * https://leetcode-cn.com/problems/house-robber-ii/description/
+ *
+ * algorithms
+ * Medium (33.59%)
+ * Likes:    113
+ * Dislikes: 0
+ * Total Accepted:    8.7K
+ * Total Submissions: 25.3K
+ * Testcase Example:  '[2,3,2]'
+ *
+ * 
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都围成一圈，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * 
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,3,2]
+ * 输出: 3
+ * 解释: 你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,2,3,1]
+ * 输出: 4
+ * 解释: 你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+ * 偷窃到的最高金额 = 1 + 3 = 4 。
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob1 = function(nums) {
+  const len = nums.length
+  if (len === 0) return 0
+  if (len === 1) return nums[0]
+
+  let pre1 = 0
+  let pre2 = nums[0]
+  let i    
+  for (i = 1; i < len; i++) {
+    const num = nums[i]
+    const temp = pre2
+    pre2 = Math.max(num + pre1, temp)
+    pre1 = temp
+  }
+
+  return pre2
+};
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  const len = nums.length
+  if (len === 0) return 0
+  if (len === 1) return nums[0]
+  return Math.max(rob1(nums.slice(0, len - 1)), rob1(nums.slice(1, len)))
+};
+
