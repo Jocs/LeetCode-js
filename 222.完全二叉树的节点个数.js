8fd6ab7114c45357858e1ba9120382e794174f43
jocs <ransixi@gmail.com>
2021-09-05 22:40:28 +0800

    Solve 530

---
+++ 222.完全二叉树的节点个数.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=javascript
+ *
+ * [222] 完全二叉树的节点个数
+ *
+ * https://leetcode-cn.com/problems/count-complete-tree-nodes/description/
+ *
+ * algorithms
+ * Medium (78.00%)
+ * Likes:    526
+ * Dislikes: 0
+ * Total Accepted:    112.3K
+ * Total Submissions: 143.9K
+ * Testcase Example:  '[1,2,3,4,5,6]'
+ *
+ * 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
+ * 
+ * 完全二叉树
+ * 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h
+ * 层，则该层包含 1~ 2^h 个节点。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,3,4,5,6]
+ * 输出：6
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：0
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：1
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点的数目范围是[0, 5 * 10^4]
+ * 0 
+ * 题目数据保证输入的树是 完全二叉树
+ * 
+ * 
+ * 
+ * 
+ * 进阶：遍历树来统计节点是一种时间复杂度为 O(n) 的简单解决方案。你可以设计一个更快的算法吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+  if (!root) return 0
+  let count = 0
+  const queue = [root]
+  while (queue.length) {
+    const node = queue.shift()
+    if (node.left) queue.push(node.left)
+    if (node.right) queue.push(node.right)
+    count++
+  }
+
+  return count
+};
+// @lc code=end
+
---
+++ 530.二叉搜索树的最小绝对差.js
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=530 lang=javascript
+ *
+ * [530] 二叉搜索树的最小绝对差
+ *
+ * https://leetcode-cn.com/problems/minimum-absolute-difference-in-bst/description/
+ *
+ * algorithms
+ * Easy (62.07%)
+ * Likes:    270
+ * Dislikes: 0
+ * Total Accepted:    76K
+ * Total Submissions: 122.4K
+ * Testcase Example:  '[4,2,6,1,3]'
+ *
+ * 给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入：
+ * 
+ * ⁠  1
+ * ⁠   \
+ * ⁠    3
+ * ⁠   /
+ * ⁠  2
+ * 
+ * 输出：
+ * 1
+ * 
+ * 解释：
+ * 最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中至少有 2 个节点。
+ * 本题与 783 https://leetcode-cn.com/problems/minimum-distance-between-bst-nodes/
+ * 相同
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var getMinimumDifference = function(root) {
+  const travel = (node, cb) => {
+    node.left && travel(node.left, cb)
+    cb(node)
+    node.right && travel(node.right, cb)
+  }
+
+  let preNode = null
+  let result = Infinity
+
+  travel(root, node => {
+    if (preNode) {
+      const delta = Math.abs(node.val - preNode.val)
+      result = Math.min(result, delta)
+    }
+
+    preNode = node
+  })
+
+  return result
+};
+// @lc code=end
+
