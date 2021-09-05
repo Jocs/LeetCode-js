8d7faa426402328a9396215f1a6bfbe99e6a0ee7
jocs <ransixi@gmail.com>
2021-09-05 19:06:50 +0800

    Solve problem 99

---
+++ 99.恢复二叉搜索树.js
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=99 lang=javascript
+ *
+ * [99] 恢复二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/recover-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (61.55%)
+ * Likes:    547
+ * Dislikes: 0
+ * Total Accepted:    68.4K
+ * Total Submissions: 111.5K
+ * Testcase Example:  '[1,3,null,null,2]'
+ *
+ * 给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。
+ * 
+ * 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,3,null,null,2]
+ * 输出：[3,1,null,null,2]
+ * 解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = [3,1,4,null,null,2]
+ * 输出：[2,1,4,null,null,3]
+ * 解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树上节点的数目在范围 [2, 1000] 内
+ * -2^31 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var recoverTree = function(root) {
+  let firstErrorNode = null
+  let secondErrorNode = null
+  let previousNode = null
+
+  const travel = (node, cb) => {
+    if (node.left) {
+      travel(node.left, cb)
+    }
+    cb(node)
+    if (node.right) {
+      travel(node.right, cb)
+    }
+  }
+
+  travel(root, node => {
+    if (previousNode) {
+      if (previousNode.val > node.val) {
+        if (!firstErrorNode) {
+          firstErrorNode = previousNode
+        }
+        secondErrorNode = node
+      }
+    }
+    previousNode = node
+  })
+
+  const temp = firstErrorNode.val
+  firstErrorNode.val = secondErrorNode.val
+  secondErrorNode.val = temp
+};
+// @lc code=end
+
